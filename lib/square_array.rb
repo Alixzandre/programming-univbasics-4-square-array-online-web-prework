7dc5568e0359b0475c5c3043396723e5c2b36488
Alexander Bispo <alexbispo173@gmail.com>
2019-09-01 03:42:43 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  array2=[]
+  while counter < array.length do
+    num = array[counter]
+    card = num ** 2
+    array2.push(card)
+    counter += 1
+  end
+  return array2
 end
