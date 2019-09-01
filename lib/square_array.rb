17f6430e4b88d98b33889d66bdcbf0fe35a4e9e9
Alexander Bispo <alexbispo173@gmail.com>
2019-09-01 03:41:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   counter = 0
   array2=[]
   while counter < array.length do
-    card = array[counter]**2
+    num = array[counter]
+    card = num ** 2
     array2.push(card)
     counter += 1
   end
